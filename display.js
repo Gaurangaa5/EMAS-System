d3cd29952037eed0afeb612bea325b56866bf05a
Gaurangaa <gaurangaa25@gmail.com>
2026-06-04 20:37:12 +0800

    Refactor Firebase integration in display.js
    Updated Firebase imports and initialization in display.js. Changed text display logic to show 'WAITING...' when no message is available.

--- display.js
+++ display.js
@@ -1,16 +1,24 @@
-import { db } from "./firebase.js";
-import { ref, onValue } from "firebase/database";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
+import { getDatabase, ref, onValue } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-database.js";
 
-const textElement = document.getElementById("displayText");
+const firebaseConfig = {
+  apiKey: "AIzaSyDTexhEk_vhwe9X0lR4_hXE2kiZ9puTx5w",
+  authDomain: "emas-system.firebaseapp.com",
+  databaseURL: "https://emas-system-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "emas-system",
+  storageBucket: "emas-system.firebasestorage.app",
+  messagingSenderId: "1004123239278",
+  appId: "1:1004123239278:web:eca24f9c6304eae643077f"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getDatabase(app);
+
+const text = document.getElementById("displayText");
 
 onValue(ref(db, "emas/display"), (snapshot) => {
-
   const data = snapshot.val();
+  console.log("DATA:", data);
 
-  if (data?.text) {
-    textElement.textContent = data.text;
-  } else {
-    textElement.textContent = "NO MESSAGE";
-  }
-
+  text.innerText = data?.text || "WAITING...";
 });
